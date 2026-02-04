bc04d68b82836329c65fc74480f0f285c6b0d53c
Sagnik Roy <sagnikk.roy@gmail.com>
2026-02-05 02:48:29 +0530

    initial

---
+++ hash.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main main(){
+string s;
+cin >> s;
+
+
+int hash[256]={0};
+
+for (int i = 0; i < s.size(); i++) {
+    hash[s[i]]++;
+}
+
+int q;
+cin >> q;
+}
